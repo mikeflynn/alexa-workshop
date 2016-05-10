f6bbd29f3ae198fc4f25007fd04063c599ce8f88
Mike Flynn <mfflynn@gmail.com>
2016-05-10 15:56:51 -0700

    Tweaked code for a quick demo screenshot.

--- studio71.go
+++ studio71.go
@@ -9,10 +9,10 @@ import (
 	alexa "github.com/mikeflynn/go-alexa/skillserver"
 )
 
-func EchoStudio71(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
+func EchoIntentHandler(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
 	switch echoReq.GetIntentName() {
 	case "Status":
-		res, err := http.Get("http://status.studio71.io/status.json")
+		res, err := http.Get("http://some.application.com/status.json")
 		if err != nil {
 			log.Printf("Error fetching status json: %v", err.Error())
 		}
@@ -21,10 +21,7 @@ func EchoStudio71(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
 
 		decoder := json.NewDecoder(res.Body)
 		var data S71Status
-		err = decoder.Decode(&data)
-		if err != nil {
-			log.Printf("Error parsing status json: %v", err.Error())
-		}
+		decoder.Decode(&data)
 
 		message := ""
 		for _, app := range data.Applications {
@@ -33,16 +30,7 @@ func EchoStudio71(echoReq *alexa.EchoRequest, echoResp *alexa.EchoResponse) {
 			}
 		}
 
-		if message == "" {
-			message = getRandom([]string{
-				"All applications are up and running normally.",
-				"Everything is good to go.",
-				"All sites are running.",
-				"Everything is normal.",
-			})
-		}
-
-		echoResp.OutputSpeech(message).EndSession(true)
+		echoResp.OutputSpeech("All applications are up and running normally.").EndSession(true)
 	default:
 		echoResp.OutputSpeech("I'm sorry, I didn't get that. Can you say that again?").EndSession(false)
 	}
